e2e22922d9a68fffba5d4d08d0308e9be0e37e1c
yangzhao <zkk258369@163.com>
2019-09-20 20:23:58 +0800

    max

--- main.c
+++ main.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+int max(int a, int b)
+{
+    if(a>b)
+    {
+        return a;
+    }
+    else
+    {
+        return b;
+    }
+}
+
 int main()
 {
     printf("Hello World"); 
